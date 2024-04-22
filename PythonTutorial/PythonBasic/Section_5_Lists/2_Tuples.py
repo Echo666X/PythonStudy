48a0b840bc7bc22d87df00ad3decb5af7792a519
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-04-23 00:41:18 +0800

    pyTutorial

---
+++ PythonTutorial/PythonBasic/Section_5_Lists/2_Tuples.py
@@ -0,0 +1,7 @@
+# in this tutorial, you’ll learn about Python tuples and how to use them effectively.
+# sometimes you want to create a list of items that cannot be changed throughout the program, tuples allows you to do that.
+# so by definition, a tuple is an immutable list.
+
+# the tuple is like a list except that it uses parenthese() instead of square brackets[]
+rgb = ('red','green','blue')
+print(rgb[0],rgb[1],rgb[2],sep=",")
