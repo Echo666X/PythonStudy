0b4c002331c0648a232a67163cf6eb9d2bd3d8cb
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-04-29 21:21:28 +0800

    pyTutorial

---
+++ PythonTutorial/PythonBasic/Section_7_Sets/4_SetIntersection.py
@@ -0,0 +1,9 @@
+# in this tutorial, you’ll learn about the Python set intersection and how to use it to intersect two or more sets.
+# it is similar to fet a union, you can use intersection() method or set intersection operator &
+# for example:
+Kevin_skills = {'C++','Java','C#'}
+Mary_skills  = {'C','C++','Java'}
+skills = Kevin_skills.intersection(Mary_skills)
+print(skills)
+skills = Kevin_skills & Mary_skills
+print(skills)
