3d30014899507630f2b8279c3e36ffcaf9dfcbce
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-05-06 23:54:53 +0800

    pyTutorial

--- PythonTutorial/PythonBasic/Section_1_Fundamentals/4_Strings.py
+++ PythonTutorial/PythonBasic/Section_1_Fundamentals/4_Strings.py
@@ -13,3 +13,14 @@ message = '"Beautiful is better than ugly.". Said Tim Peters'
 print(message)
 message = 'It\'s also a valid string'
 print(message)
+
+# when building a program, you always need to check the type of a string, you can use is__() method like this:
+# isalpha(): 检查字符串是否只包含字母。
+# isdigit(): 检查字符串是否只包含数字。
+# isalnum(): 检查字符串是否只包含字母和数字，不包含空格或其他特殊字符。
+# isspace(): 检查字符串是否只包含空格、制表符、换行符等空白字符。
+# islower(): 检查字符串是否全部由小写字母组成。
+# isupper(): 检查字符串是否全部由大写字母组成。
+# istitle(): 检查字符串中的每个单词是否首字母大写，其余字母小写（标题格式）。
+# startswith(prefix): 检查字符串是否以指定的前缀开头。
+# endswith(suffix): 检查字符串是否以指定的后缀结尾。
