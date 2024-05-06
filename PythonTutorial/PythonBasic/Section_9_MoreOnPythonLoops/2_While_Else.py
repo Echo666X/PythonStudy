167da859bb5f0216b778ee1a62ad771729fcf383
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-05-06 15:50:56 +0800

    pyTutorial

--- PythonTutorial/PythonBasic/Section_9_MoreOnPythonLoops/2_While_Else.py
+++ PythonTutorial/PythonBasic/Section_9_MoreOnPythonLoops/2_While_Else.py
@@ -1,4 +1,4 @@
 # in this tutorial, you’ll learn about the Python while else statement and how to use it effectively.
 # in python, the condition is checked at the beginning of each iteration, the code block inside the while statement will execute as long as the condition is true
 # when the condition becomes false and the loop runs normally, the else clause will execute
-# if the loop is terminated permaturely by either a break or return statement, the else clause won't execute at all.
+# if the loop is terminated permaturely by either a break or return statement, the else clause won't execute.
