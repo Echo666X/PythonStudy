75213bc6ebf910ee5b2621b6d12ac4961ad00cbe
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-05-22 03:03:05 +0800

    pyTestCode

---
+++ class/test5.19.2.py
@@ -0,0 +1,10 @@
+def main():
+    n = int(input())
+    if n == 0 :
+        print('0 indian boy.')
+    elif n == 1:
+        print('1 indian boy.')
+    elif n>1 :
+        print(f"{n} indian boys.")
+        
+main()
