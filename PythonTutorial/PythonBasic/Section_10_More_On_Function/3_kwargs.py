fdafd80c364cdd7f828091625ae3ea0b2d669965
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-05-22 03:03:54 +0800

    pyTutorial

---
+++ PythonTutorial/PythonBasic/Section_10_More_On_Function/3_kwargs.py
@@ -0,0 +1,3 @@
+# in this tutorial, you’ll learn about the Python **kwargs parameters.
+# in python, a function can have a parameter preceded by two stars(**), 
+# the **kwargs is called a keyword parameter
