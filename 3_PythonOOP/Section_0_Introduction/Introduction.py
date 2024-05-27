58d1133f518683e564cfc296537ec98d3e8d6d85
Echo666X <144587923+Echo666X@users.noreply.github.com>
2024-05-27 23:50:17 +0800

    pyOOP

---
+++ 3_PythonOOP/Section_0_Introduction/Introduction.py
@@ -0,0 +1,8 @@
+# This Python OOP explains to you the Python object-oriented programming clearly so that you can apply it to develop software more effectively.
+
+# By the end of this Python OOP module, you’ll have good knowledge of object-oriented principles. And you’ll know how to use Python syntax to create reliable and robust software applications.
+
+# What you’ll learn
+    # Create objects in Python by defining classes and methods.
+    # Extend classes using inheritance.
+    # SOLID principles in object-oriented programming.
